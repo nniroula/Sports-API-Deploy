5f06b94139965bf70c01d96514bed99b02916e76
nabin <nkbhaie2017@gmail.com>
2023-01-06 11:17:30 -0700

    initial commit

--- configs/configurations.js
+++ configs/configurations.js
@@ -2,34 +2,14 @@
 
 const express = require('express');
 
-/** Shared configurations for application, and is required at many places. */
-
 require("dotenv").config();
-// require("colors");
-const SECRET_KEY = "secrettocricketaecc";
-// const SECRET_KEY = process.env.stripesecretkey || "secrettocricketaecc";
-
+const SECRET_KEY = process.env.stripesecretkey || "secrettocricketaecc2020";
 
 const PORT = +process.env.PORT || 3000;
-// const PORT = 3000;
-
-// Use dev database, testing database, or via env var, production database
-// function getDatabaseUri() {
-//   return (process.env.NODE_ENV === "test")
-//       ? "jobly_test"
-//       : process.env.DATABASE_URL || "jobly";
-// }
-
-// Speed up bcrypt during tests, since the algorithm safety isn't being tested
-//
-// WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
-
-// console.log("PORT:", PORT.toString());
 
 module.exports = {
     PORT,
     BCRYPT_WORK_FACTOR,
     SECRET_KEY,
-    // getDatabaseUri,
 };
--- db.js
+++ db.js
@@ -4,7 +4,7 @@ let DB_URI;
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-DB_URI = "postgresql:///aecc_test_db";
+DB_URI = "postgresql:///aecc_db";
 
 let db = new Client({
     connectionString: isProduction ? process.env.DATABASE_URL : DB_URI,
